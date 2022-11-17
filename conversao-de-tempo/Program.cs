e136552d69df6175be0161e9015af37eb1e8b514
abraao 284 <abraao-284@outlook.com>
2022-11-17 15:13:15 -0300

    desafios intermediarios

---
+++ conversao-de-tempo/Program.cs
@@ -0,0 +1,6 @@
+﻿var timeInSeconds = int.Parse(Console.ReadLine());
+var hours = timeInSeconds/3600;
+timeInSeconds = timeInSeconds % (3600);
+var minutes = timeInSeconds /60;
+var seconds = timeInSeconds % 60;
+Console.WriteLine($"{hours}:{minutes}:{seconds}");
